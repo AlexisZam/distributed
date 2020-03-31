d095d19ca8ab13076363fcbd60eb30aa70652021
alexiszam <alexiszamanis1997@gmail.com>
2020-04-01 00:31:19 +0300

    fix

--- src/backend/test.py
+++ src/backend/test.py
@@ -31,8 +31,7 @@ with open(
         index, amount = line.split()
         index = int(index[2:])
         data = {"receiver_public_key": public_keys[index], "amount": int(amount)}
-        print(data)
-        post(f"http://{address}/create_transaction", data=dumps(data))
+        post(f"http://{address}/transaction", data=dumps(data))
 
 balance = loads(get(f"http://{address}/balance").content)
 print(balance)
