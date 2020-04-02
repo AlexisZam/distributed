91378af74d4d79dea941e5b532102b67a6360d01
alexiszam <alexiszamanis1997@gmail.com>
2020-04-03 02:03:37 +0300

    minor fix

--- src/server/config.py
+++ src/server/config.py
@@ -1,15 +1,15 @@
 from argparse import ArgumentParser
 
-BOOTSTRAP_ADDRESS = "192.168.1.1:5000"
-
 parser = ArgumentParser(add_help=False)
 parser.add_argument("-h", "--host", default="192.168.1.1", type=str)
 parser.add_argument("-p", "--port", default=5000, type=int)
-parser.add_argument("-c", "--capacity", default=1, type=int)
-parser.add_argument("-d", "--difficulty", default=1, type=int)
-parser.add_argument("-n", "--n_nodes", default=1, type=int)
+parser.add_argument("-b", "--bootstrap_address", default="192.168.1.1:5000", type=str)
+parser.add_argument("-c", "--capacity", default=2, type=int)
+parser.add_argument("-d", "--difficulty", default=4, type=int)
+parser.add_argument("-n", "--n_nodes", default=3, type=int)
 args = parser.parse_args()
 
+BOOTSTRAP_ADDRESS = args.bootstrap_address
 HOST = args.host
 PORT = args.port
 CAPACITY = args.capacity
