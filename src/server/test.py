10334c95792abbc5e458e1ec790b21ed47969b10
alexiszam <alexiszamanis1997@gmail.com>
2020-04-02 20:18:18 +0300

    major fix

--- src/server/test.py
+++ src/server/test.py
@@ -25,7 +25,7 @@ with open(
         index, amount = map(int, line[2:].split())
         post(f"http://{address}/transaction", data=dumps((public_keys[index], amount)))
 
-average_block_time = loads(get(f"http://{address}/average_block_time").content)
+average_block_time = loads(get(f"http://{address}/metrics/average_block_time").content)
 print(average_block_time)
 
 balance = loads(get(f"http://{address}/balance").content)
