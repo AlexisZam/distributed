e963cab5bd01f6db9802cd4657f0713d6303010c
alexiszam <alexiszamanis1997@gmail.com>
2020-04-03 02:04:13 +0300

    minor change

--- src/server/config.py
+++ src/server/config.py
@@ -1,9 +1,9 @@
 from argparse import ArgumentParser
 
 parser = ArgumentParser(add_help=False)
-parser.add_argument("-h", "--host", default="192.168.1.1", type=str)
+parser.add_argument("-h", "--host", default="127.0.0.1", type=str)
 parser.add_argument("-p", "--port", default=5000, type=int)
-parser.add_argument("-b", "--bootstrap_address", default="192.168.1.1:5000", type=str)
+parser.add_argument("-b", "--bootstrap_address", default="127.0.0.1:5000", type=str)
 parser.add_argument("-c", "--capacity", default=2, type=int)
 parser.add_argument("-d", "--difficulty", default=4, type=int)
 parser.add_argument("-n", "--n_nodes", default=3, type=int)
