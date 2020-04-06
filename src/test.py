43d9d1662681b2270bc780b7e00d810ce178b769
alexiszam <alexiszamanis1997@gmail.com>
2020-04-06 20:10:35 +0300

    fix

--- src/test.py
+++ src/test.py
@@ -8,16 +8,18 @@ from time import sleep
 from requests import get, post
 
 parser = ArgumentParser(add_help=False)
-parser.add_argument("-a", "--address", default="127.0.0.1:5000", type=str)
+parser.add_argument("-h", "--host", default="127.0.0.1", type=str)
+parser.add_argument("-p", "--port", default=5000, type=int)
 parser.add_argument("-n", "--n_nodes", default=1, type=int)
 args = parser.parse_args()
+address = f"{args.host}:{args.port}"
 
 while True:
-    public_keys = get(f"http://{args.address}/public_keys").json()
+    public_keys = get(f"http://{address}/public_keys").json()
     if len(public_keys) == args.n_nodes:
         break
 
-addresses = get(f"http://{args.address}/addresses").json()
+addresses = get(f"http://{address}/addresses").json()
 while True:
     if all(
         all(balance == 100 for balance in get(f"http://{address}/balances").json())
@@ -27,7 +29,7 @@ while True:
     sleep(5)
 
 sleep(5)
-index = get(f"http://{args.address}/index").json()
+index = get(f"http://{address}/index").json()
 
 with open(
     f"/home/user/distributed/transactions/{ceil(args.n_nodes / 5) * 5}nodes/transactions{index}.txt"
@@ -36,7 +38,7 @@ with open(
         index, amount = map(int, line[2:].split())
         if index < args.n_nodes:
             post(
-                f"http://{args.address}/transaction",
+                f"http://{address}/transaction",
                 json={"receiver_public_key": public_keys[index], "amount": amount},
             )
 
@@ -50,13 +52,13 @@ with open(
 #         counter = 0
 #     sleep(1)
 
-prev_balances = get(f"http://{args.address}/balances").json()
-prev_committed_balances = get(f"http://{args.address}/balances").json()
+prev_balances = get(f"http://{address}/balances").json()
+prev_committed_balances = get(f"http://{address}/balances").json()
 n_equals = 0
 while True:
     sleep(5)
-    curr_balances = get(f"http://{args.address}/balances").json()
-    curr_committed_balances = get(f"http://{args.address}/balances").json()
+    curr_balances = get(f"http://{address}/balances").json()
+    curr_committed_balances = get(f"http://{address}/balances").json()
     if (
         curr_balances == prev_balances
         and curr_committed_balances == prev_committed_balances
@@ -69,21 +71,21 @@ while True:
     prev_balances = curr_balances
     prev_committed_balances = curr_committed_balances
 
-average_block_time = get(f"http://{args.address}/metrics/average_block_time").json()
+average_block_time = get(f"http://{address}/metrics/average_block_time").json()
 print("Average block time:", average_block_time)
 
-average_throughput = get(f"http://{args.address}/metrics/average_throughput").json()
+average_throughput = get(f"http://{address}/metrics/average_throughput").json()
 print("Average throughput:", average_throughput)
 
-statistics = get(f"http://{args.address}/metrics/statistics").json()
+statistics = get(f"http://{address}/metrics/statistics").json()
 print("Statistics:", statistics)
 
-balances = get(f"http://{args.address}/balances").json()
+balances = get(f"http://{address}/balances").json()
 print("Balances:", balances)
 assert sum(balances) == args.n_nodes * 100
 
-committed_balances = get(f"http://{args.address}/committed_balances").json()
+committed_balances = get(f"http://{address}/committed_balances").json()
 print("Committed balances:", committed_balances)
 assert sum(committed_balances) == args.n_nodes * 100
 
-post(f"http://{args.address}/quit")
+post(f"http://{address}/quit")
