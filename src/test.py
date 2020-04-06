bb9d6b64bcca9c51f63264d87aa6292a96674b1a
alexiszam <alexiszamanis1997@gmail.com>
2020-04-06 19:54:53 +0300

    fix

--- src/test.py
+++ src/test.py
@@ -13,44 +13,22 @@ parser.add_argument("-n", "--n_nodes", default=1, type=int)
 args = parser.parse_args()
 
 while True:
-    r = get(f"http://{args.address}/public_keys")
-    r.raise_for_status()
-    if len(r.json()) == args.n_nodes:
+    public_keys = get(f"http://{args.address}/public_keys").json()
+    if len(public_keys) == args.n_nodes:
         break
-    sleep(1)
-public_keys = r.json()
 
 addresses = get(f"http://{args.address}/addresses").json()
 while True:
-    rs = [get(f"http://{address}/busy") for address in addresses]
-    for r in rs:
-        r.raise_for_status()
-    if not any(r.json() for r in rs):
-        break
-    sleep(1)
-
-r = get(f"http://{args.address}/balances")
-r.raise_for_status()
-assert all(balance == 100 for balance in r.json())
-
-sleep(5)
-
-public_keys = get(f"http://{args.address}/public_keys").json()
-index = get(f"http://{args.address}/index").json()
-
-assert len(public_keys) == args.n_nodes
-
-for _ in range(10):
-    balances = get(f"http://{args.address}/balances").json()
-    if all(balance == 100 for balance in balances):
+    if all(
+        all(balance == 100 for balance in get(f"http://{address}/balances").json())
+        for address in addresses
+    ):
         break
     sleep(5)
 
-assert all(balance == 100 for balance in balances)
-
 sleep(5)
-
 index = get(f"http://{args.address}/index").json()
+
 with open(
     f"/home/user/distributed/transactions/{ceil(args.n_nodes / 5) * 5}nodes/transactions{index}.txt"
 ) as f:
@@ -61,20 +39,16 @@ with open(
                 f"http://{args.address}/transaction",
                 json={"receiver_public_key": public_keys[index], "amount": amount},
             )
-            print(index, amount)
 
-while True:
-    rs = [get(f"http://{address}/busy") for address in addresses]
-    for r in rs:
-        r.raise_for_status()
-    if not any(r.json() for r in rs):
-        sleep(1)
-        rs = [get(f"http://{address}/busy") for address in addresses]
-        for r in rs:
-            r.raise_for_status()
-        if not any(r.json() for r in rs):
-            break
-    sleep(1)
+# counter = 0
+# while True:
+#     if not any(get(f"http://{address}/busy").json() for address in addresses):
+#         counter += 1
+#         if counter == 5:
+#             break
+#     else:
+#         counter = 0
+#     sleep(1)
 
 prev_balances = get(f"http://{args.address}/balances").json()
 prev_committed_balances = get(f"http://{args.address}/balances").json()
@@ -96,7 +70,6 @@ while True:
     prev_committed_balances = curr_committed_balances
 
 average_block_time = get(f"http://{args.address}/metrics/average_block_time").json()
-
 print("Average block time:", average_block_time)
 
 average_throughput = get(f"http://{args.address}/metrics/average_throughput").json()
